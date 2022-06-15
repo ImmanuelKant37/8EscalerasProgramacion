7ec5b68aa0493ca4401e8003d4e3864b2d6ebcef
ImmanuelKant37 <Fecsoul@gmail.com>
2022-06-15 19:57:59 -0300

    Update historias_clinicas.py
    Adaptaciones para escalar

--- Instituto medico Las luciernagas/historias_clinicas.py
+++ Instituto medico Las luciernagas/historias_clinicas.py
@@ -1,12 +1,12 @@
 import json
 
 Historias_clinicas = [] #lo primero que hay que crear antes de crer el json 
-for Registro_de_historias_clinicas in range (1):
+for Registro_de_historias_clinicas in range (1): #Parametrizable, hacer funcion para adaptar
     H_clinicas= {}
-    Fecha = input ("Ingresar fecha de consulta: ")
+    Fecha = input ("Ingresar fecha de consulta: ") #Para validar tipo Fecha valida
     Condicion_medica = input ("Condicion medica: ")
     Medico_del_insituto= input ("Ingrese nombre del medico que lo trató: ")
-    Especialidad_del_medico = input ("Ingresar especialidad del medico: ")
+    Especialidad_del_medico = input ("Ingresar especialidad del medico: ") 
     Observaciones = input( "Ingresar observaciones generales: ")
     H_clinicas ["fecha"] = Fecha
     H_clinicas ["Condicion_medica"] = Condicion_medica
