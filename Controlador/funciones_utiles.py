2467f7218130ef94ccc852d063d5a7b35cb8b6e0
ImmanuelKant37 <Fecsoul@gmail.com>
2022-06-15 22:04:00 -0300

    Add files via upload
    Proyecto en formato bucle

---
+++ Controlador/funciones_utiles.py
@@ -0,0 +1,12 @@
+
+def es_cero(entrada):
+    if entrada==0:
+        return True
+
+def es_uno(entrada):
+    if entrada==1:
+        return True
+
+def es_dos(entrada):
+    if entrada==2:
+        return True
